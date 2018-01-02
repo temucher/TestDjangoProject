9e7f24489cb296ba7fc7dea052bc6a166742e94d
temucher <tjsmuch@gmail.com>
2018-01-02 14:54:54 -0800

    Added a view and a template for a detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),  # potentiall use path, following tutorial and pretty sure this is an outdated method
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
